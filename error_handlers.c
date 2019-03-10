0e9f0324d98bd2e71a1d22c66115597854e044cc
Kees Cook <kees@outflux.net>
2019-03-10 14:27:24 -0700

    logging: remove mention of original upstream
    Do not request that bugs get reported to the original upstream, since
    they have abandoned the project.
    
    Signed-off-by: Kees Cook <kees@outflux.net>

--- error_handlers.c
+++ error_handlers.c
@@ -5,19 +5,10 @@
 
 void fatal_error(char *msg)
 {
-   char temp_buf[512];
-
    if (datafile != NULL)
       unload_datafile(datafile);
 
-   sprintf(temp_buf, "\nERROR: %s", msg);
-   strcat(temp_buf, "\n\nPlease contact ScanTool.net via our website at http://www.ScanTool.net/support");
-   strcat(temp_buf, "\nInclude the following information:");
-   strcat(temp_buf, "\n\t- Exact error message");
-   strcat(temp_buf, "\n\t- CPU type/speed, i.e. \"Pentium 100Mhz\"");
-   strcat(temp_buf, "\n\t- OS, i.e. \"Windows 95\"");
-   strcat(temp_buf, "\n\t- Total amount of RAM installed, i.e. \"4Mb\"\n\n");
-   allegro_message("%s", temp_buf);
+   allegro_message("\nERROR: %s\n", msg);
    
    exit(EXIT_FAILURE);
 }
