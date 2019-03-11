b0119546432e4c9def7c21b6531897b2a6167842
Kees Cook <kees@outflux.net>
2019-03-11 16:17:59 -0700

    version: Bump to 2.0
    Signed-off-by: Kees Cook <kees@outflux.net>

--- version.h
+++ version.h
@@ -1,20 +1,24 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define SCANTOOL_VERSION          1
-#define SCANTOOL_SUB_VERSION      21
+#define SCANTOOL_VERSION          2
+#define SCANTOOL_SUB_VERSION      0
 #define SCANTOOL_WIP_VERSION      0
-#define SCANTOOL_VERSION_STR      "1.21"
-#define SCANTOOL_VERSION_EX_STR   "1.21"
-#define SCANTOOL_YEAR_STR         "2010"
-#define SCANTOOL_DATE             20100623    /* yyyymmdd */
+#define SCANTOOL_VERSION_STR      "2.0"
+#define SCANTOOL_VERSION_EX_STR   "2.0"
+#define SCANTOOL_YEAR_STR         "2019"
+#define SCANTOOL_DATE             20190311    /* yyyymmdd */
 
 #ifdef ALLEGRO_WINDOWS
    #define SCANTOOL_PLATFORM_STR   "Windows"
 #else
+ #ifdef TERMIOS
+   #define SCANTOOL_PLATFORM_STR   "POSIX"
+ #else
    #define SCANTOOL_PLATFORM_STR   "DOS"
+ #endif
 #endif
 
-#define SCANTOOL_COPYRIGHT_STR     "Copyright © " SCANTOOL_YEAR_STR " ScanTool.net, LLC"
+#define SCANTOOL_COPYRIGHT_STR     "Copyright © " SCANTOOL_YEAR_STR
 
 #endif
