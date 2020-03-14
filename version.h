e53109a549312241da363d3bbdb4f231f1ba7a42
Kees Cook <kees@outflux.net>
2020-03-14 09:43:46 -0700

    version: prepare to release 2.1
    The fixes for -lm and missing CPPFLAGS are sufficient for a minor release.

--- version.h
+++ version.h
@@ -2,12 +2,12 @@
 #define VERSION_H
 
 #define SCANTOOL_VERSION          2
-#define SCANTOOL_SUB_VERSION      0
+#define SCANTOOL_SUB_VERSION      1
 #define SCANTOOL_WIP_VERSION      0
-#define SCANTOOL_VERSION_STR      "2.0"
-#define SCANTOOL_VERSION_EX_STR   "2.0"
-#define SCANTOOL_YEAR_STR         "2019"
-#define SCANTOOL_DATE             20190311    /* yyyymmdd */
+#define SCANTOOL_VERSION_STR      "2.1"
+#define SCANTOOL_VERSION_EX_STR   "2.1"
+#define SCANTOOL_YEAR_STR         "2020"
+#define SCANTOOL_DATE             20200314    /* yyyymmdd */
 
 #ifdef ALLEGRO_WINDOWS
    #define SCANTOOL_PLATFORM_STR   "Windows"
